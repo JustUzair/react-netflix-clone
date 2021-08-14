02f46bfb41948a71305398d0799ad8cfa8f6bde7
JustUzair <67183061+JustUzair@users.noreply.github.com>
2021-08-14 20:07:09 +0530

    Update Request.js

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,4 +1,4 @@
-// const API_KEY = process.env.REACT_APP_TMDB_API;
+
 const API_KEY = process.env.REACT_APP_TMDB_API;
 const genres = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
